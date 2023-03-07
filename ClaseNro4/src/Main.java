686901140db2befad242d6a3f8dabafffc298a97
AdrianC2023 <adrian.colque91@gmail.com>
2023-03-07 11:45:22 -0300

    Se creo como ejemplo el producto pan y su descripcion

--- ClaseNro4/src/Main.java
+++ ClaseNro4/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        Producto productoPan = new Producto("Pan");
+        System.out.println(productoPan.getNombre());
+
+        productoPan.setDescripcion("integral en molde");
+        System.out.println(productoPan.getDescripcion());
     }
 }
