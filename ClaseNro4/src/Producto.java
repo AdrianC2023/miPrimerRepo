9d3edc27ad59f212507fd5bbcd90b45071d24dd8
AdrianC2023 <adrian.colque91@gmail.com>
2023-03-07 02:40:19 -0300

    Se creo la clase Persona

---
+++ ClaseNro4/src/Producto.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+
+public class Producto {
+
+    private String nombre;
+    private String descripcion;
+    private LocalDate fechaAlta;
+    private Integer pesoKg;
+    private Double precio;
+
+    private static final Double minPrecio = 0.1;
+
+    public Producto(String nombre){
+        this.nombre = nombre;
+        this.fechaAlta = LocalDate.now();
+        this.precio = minPrecio;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+    public void setDescripcion(String descripcion){
+        this.descripcion = descripcion;
+    }
+}
