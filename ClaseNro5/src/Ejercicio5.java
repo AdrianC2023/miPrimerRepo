b1c6bf11bd5968e29c8e60a86e68048b01d14a24
AdrianC2023 <adrian.colque91@gmail.com>
2023-03-26 20:55:11 -0300

    Ejercicio 5 se ordeno el codigo. Se borraron instrucciones comentadas que estaban de mas

--- ClaseNro5/src/Ejercicio5.java
+++ ClaseNro5/src/Ejercicio5.java
@@ -10,13 +10,7 @@ public class Ejercicio5{
     public static void main(String[] args) throws IOException {
 
         System.out.println("<<<Ejercicio 2>>>");
-        //System.out.println(args.length);
-        //List<String> nros;
 
-        /*for (int i = 0; i < args.length; i++) {
-            System.out.println("hola");
-            System.out.println(args[0]);
-        }*/
         String unArch = "./ClaseNro5/recursos/numeros.txt";
         String cadena = null;
         for (String linea : Files.readAllLines(Paths.get(unArch))) {
@@ -24,7 +18,7 @@ public class Ejercicio5{
             cadena = linea;
             System.out.println("Numeros del archivo: "+cadena);
         }
-        //System.out.println(cadena);
+
         int acu=0;
         int multi=1;
         for (String j : cadena.split(",")){
@@ -33,8 +27,6 @@ public class Ejercicio5{
             acu=acu+n;
             multi=multi*n;
         }
-        //System.out.println("suma: "+acu);
-        //System.out.println("multiplicacion: "+multi);
 
         System.out.println("1 - Mostrar suma ");
         System.out.println("2 - Mostrar multiplicacion ");
@@ -53,20 +45,13 @@ public class Ejercicio5{
             default:
         }
 
-        //String mensaje;// = "cara y cruz";
-        String dicc = "abcdefghijklmnopqrstuvwxyz ";
-        //String cifrado = "           ";
-        //char[] let = mensaje.toCharArray();
-        char[] d = dicc.toCharArray();
-        //char[] cif = cifrado.toCharArray();
-
-        //String mensaje2 = "ectcb betwa";
-        String dicc2 = "abcdefghijklmnopqrstuvwxyz ";
-        //String cifrado2 = "           ";
-        //char[] let2 = mensaje2.toCharArray();
-        char[] d2 = dicc2.toCharArray();
-        //char[] cif2 = cifrado2.toCharArray();
         System.out.print("<<<Ejercicio 3>>>\n");
+
+        String dicc = "abcdefghijklmnopqrstuvwxyz ";
+        char[] d = dicc.toCharArray();
+
+        String dicc2 = "abcdefghijklmnopqrstuvwxyz ";
+        char[] d2 = dicc2.toCharArray();
 
         System.out.println("1 - Codificacion ");
         System.out.print("2 - Decodificacion \n");
@@ -83,9 +68,7 @@ public class Ejercicio5{
                 String cifrado = "           ";
 
                 for (String linea : Files.readAllLines(Paths.get(archIn))) {
-                    //System.out.println(linea);
                     System.out.println("Mensaje del archivo de entrada: "+(mensaje = linea));
-                    //System.out.println(mensaje);
                 }
                 char[] let = mensaje.toCharArray();
                 char[] cif = cifrado.toCharArray();
@@ -105,7 +88,6 @@ public class Ejercicio5{
                             }else {
                                 pos = j + desp;
                                 cif[i] = d[pos];
-                                //System.out.println("posicion letra: " + j + " " + d[j] + " to " + cif[i]);
                             }
                         }
                     }
@@ -117,9 +99,6 @@ public class Ejercicio5{
                 for (String linea : Files.readAllLines(Paths.get(archOut))) {
                     System.out.println("Mensaje del archivo de salida: "+(mensajeCifrado));
                 }
-                //System.out.println("Mensaje: " + mensaje);
-                //String mensajeCifrado = new String(cif);
-                //System.out.print("Mensaje codificado: " + mensajeCifrado + "\n");
                 break;
             case 2:
                 System.out.print("Ingrese desplazamiento: ");
@@ -130,9 +109,7 @@ public class Ejercicio5{
                 String cifrado2 = "           ";
 
                 for (String linea : Files.readAllLines(Paths.get(archOut2))) {
-                    //System.out.println(linea);
                     System.out.println("Mensaje del archivo de salida: "+(mensaje2 = linea));
-                    //System.out.println(mensaje);
                 }
 
                 char[] let2 = mensaje2.toCharArray();
@@ -153,7 +130,6 @@ public class Ejercicio5{
                             }else {
                                 pos2 = j - desp2;
                                 cif2[i] = d2[pos2];
-                                //System.out.println("posicion letra: " + j + " " + d2[j] + " to " + cif[i]);
                             }
                         }
                     }
@@ -165,9 +141,6 @@ public class Ejercicio5{
                 for (String linea : Files.readAllLines(Paths.get(archIn2))) {
                     System.out.println("Mensaje del archivo de entrada: "+(mensajeCifrado2));
                 }
-                //System.out.println("Mensaje: " + mensaje2);
-                //String mensajeCifrado2 = new String(cif2);
-                //System.out.print("Mensaje codificado: " + mensajeCifrado2 + "\n");
                 break;
             default:
         }
