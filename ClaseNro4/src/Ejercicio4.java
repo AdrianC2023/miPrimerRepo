6f671f5b72181d49a42b44b2a30d0db19ae769d5
AdrianC2023 <adrian.colque91@gmail.com>
2023-03-26 23:52:35 -0300

    Ejercicio 4 se reviso el codigo y se borraron intrucciones comentadas

--- ClaseNro4/src/Ejercicio4.java
+++ ClaseNro4/src/Ejercicio4.java
@@ -16,38 +16,5 @@ public class Ejercicio4 {
         carrito1.agregarProducto(producto3);
 
         System.out.println("Importe total: "+carrito1.importeTotal());
-
-        /*Scanner entrada = new Scanner(System.in);
-        String nom;
-        Double pre;
-        int numeroProductos;
-        Double sum = 0.0;
-
-        System.out.print("Cantidad de productos: ");
-        numeroProductos = entrada.nextInt();
-
-        Producto prod[] = new Producto[numeroProductos];
-
-        for (int i=0; i<prod.length; i++){
-            entrada.nextLine();
-            System.out.print("Nombre del producto "+(i+1)+": ");
-            nom = entrada.nextLine();
-            System.out.print("Precio: ");
-            pre = entrada.nextDouble();
-
-            prod[i] = new Producto(nom, pre);
-            sum = sum + (prod[i].getPrecio());
-        }
-            System.out.print("Monto total: "+sum);*/
-
-        //Producto productoPan = new Producto("Pan", 300.20);
-        //System.out.println(productoPan.getNombre());
-        //System.out.println(productoPan.getPrecio());
-
-        //productoPan.setDescripcion("integral en molde");
-        //System.out.println(productoPan.getDescripcion());
-
-        //productoPan.setPrecio(300.20);
-        //System.out.println(productoPan.getPrecio());
     }
 }
